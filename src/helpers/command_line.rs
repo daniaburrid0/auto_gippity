1a4c713a8e5a2f50222a52c2e9362ed3b5044a38
Daniel Barrientos <danielbarrientos68@gmail.com>
2023-12-07 00:39:29 -0300

    First files and setups

---
+++ src/helpers/command_line.rs
@@ -0,0 +1,27 @@
+use crossterm::{
+    style::{Color, Print, ResetColor, SetForegroundColor},
+    ExecutableCommand,
+};
+use std::io::{stdin, stdout};
+
+// Get user request
+pub fn get_user_response(question: &str) -> String {
+    let mut stdout: std::io::Stdout = stdout();
+
+    // Print question in an espedific color
+    stdout.execute(SetForegroundColor(Color::Blue)).unwrap();
+    println!("");
+    println!("{}", question);
+
+    // reset color
+    stdout.execute(ResetColor).unwrap();
+
+    // read user input
+    let mut user_response = String::new();
+    stdin()
+    .read_line(&mut user_response)
+    .expect("Failed to read line");
+
+    // trim whitespaces and return
+    return user_response.trim().to_string();
+}
